d8c0393664492989f7b327b37e60f6b48188e25c
Davide Carpini <davidecarpini@MacBook-Air-di-Davide.local>
2019-06-19 11:28:01 +0200

    mail sender v1

--- config.ts
+++ config.ts
@@ -2,15 +2,29 @@ import convict from 'convict';
 
 export default convict({
   server: {
-    host: {
-      format: String,
-      default: 'http://localhost',
-      env: 'SERVER_HOST'
-    },
     port: {
       format: 'port',
-      default: 3000,
+      default: 30201,
       env: 'SERVER_PORT'
+    }
+  },
+  mail: {
+    test: {
+      format: Boolean,
+      default: true,
+      env: 'MAIL_TEST'
     },
+    smtp: {
+      host: {
+        format: String,
+        default: 'smtp.sdabocconi.it',
+        env: 'SMTP_HOST'
+      },
+      port: {
+        format: 'port',
+        default: 25,
+        env: 'SMTP_HOST'
+      }
+    }
   }
-})
+});
--- server/routes.ts
+++ server/routes.ts
@@ -1,11 +1,49 @@
 import Router from 'koa-router';
 import { logger } from '../logger';
+import mailer from 'nodemailer';
 const router = new Router();
+import config from '../config';
 
 router
-  .get('/', (ctx) => {
-    logger.info('Hello!');
-    ctx.body = 'Hello!'
+  .get('/', ctx => {
+    ctx.body = 'mail-sender online!';
   })
+  .post('/send', ctx => {
+    logger.info('mail data: ', ctx.request.body);
+    console.log(config.get('mail.test'));
+    ctx.body = 'mail-sender online!';
+    let options: any;
+    if (config.get('mail.test')) {
+      options = {
+        host: 'smtp.mailtrap.io',
+        port: 2525,
+        auth: {
+          user: 'd566a1d237c40b',
+          pass: 'cf25bd4f0bca36'
+        }
+      };
+    } else {
+      options = {
+        host: config.get('mail.smtp.host'),
+        port: config.get('mail.smtp.port')
+      };
+    }
+    const transporter = mailer.createTransport(options);
+    transporter.verify(error => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Server is ready to send messages');
+      }
+    });
+    const data = ctx.request.body;
+    ctx.body = transporter.sendMail(data, err => {
+      if (err) {
+        return err;
+      } else {
+        return `sended mail: ${data}`;
+      }
+    });
+  });
 
 export default router;
